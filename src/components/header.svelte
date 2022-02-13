05ced62ba46efc1745af50e880ac419340827d26
xTorettox <fg.garciacendra@gmail.com>
2022-02-13 01:01:50 -0300

    Hello world!!! (plus header half done)

---
+++ src/components/header.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import {
+		Collapse,
+		Navbar,
+		NavbarToggler,
+		NavbarBrand,
+		Nav,
+		NavItem,
+		NavLink,
+		Dropdown,
+		DropdownToggle,
+		DropdownMenu,
+		DropdownItem
+	} from 'sveltestrap';
+
+	let isOpen = false;
+
+	function handleUpdate(event) {
+		isOpen = event.detail.isOpen;
+	}
+</script>
+
+<head>
+	<!-- importación de Bootstrap para trabajar con Sveltestrap -->
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+	/>
+	<!-- importación de Google Fonts - Staatliches -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Staatliches" />
+</head>
+
+<div style="font-family: 'Staatliches', serif; font-size:xx-large;">
+	<img src="logo_universal.png" alt="logo universal servicios" height="80" />
+	UNIVERSAL SERVICIOS
+</div>
+<Navbar color="light" light expand="md">
+	<NavbarBrand href="/">Barrita wii</NavbarBrand>
+	<NavbarToggler on:click={() => (isOpen = !isOpen)} />
+	<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+		<Nav class="ml-auto" navbar>
+			<NavItem>
+				<NavLink href="#components/">Components</NavLink>
+			</NavItem>
+			<NavItem>
+				<NavLink href="https://github.com/bestguy/sveltestrap">GitHub</NavLink>
+			</NavItem>
+			<Dropdown nav inNavbar>
+				<DropdownToggle nav caret>Options</DropdownToggle>
+				<DropdownMenu right>
+					<DropdownItem>Option 1</DropdownItem>
+					<DropdownItem>Option 2</DropdownItem>
+					<DropdownItem divider />
+					<DropdownItem>Reset</DropdownItem>
+				</DropdownMenu>
+			</Dropdown>
+		</Nav>
+	</Collapse>
+</Navbar>
---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Header from '../components/header.svelte';
+</script>
+
+<title>JulitoSoft - Soft de Taller para Julito</title>
+<main>
+	<div class="container" style="background-color: white;">
+		<Header />
+	</div>
+</main>
+
+<style>
+	main {
+		background-color:lightsteelblue;
+	}
+</style>
